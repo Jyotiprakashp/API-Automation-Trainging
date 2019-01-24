103fefb1328f3ff529ad672d175f5189f1efd45b
Jyotiprakashp <47008603+Jyotiprakashp@users.noreply.github.com>
2019-01-25 01:03:31 +0530

    Add files via upload

---
+++ FirstJavaProgram.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package api_automation;
+
+/**
+ * @author Bubu
+ *
+ */
+public class FirstJavaProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello to API Autimation");
+		System.out.println("There is a change");
+	}
+
+}
